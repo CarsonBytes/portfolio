98e7a0b56c2b1849c7947aba0e584ed45dd36bf0
Carson Ng <carsonng2000@gmail.com>
2023-11-02 01:10:03 +0800

    added demo examples

---
+++ demo/index.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Carson Ng</title>
+    <!-- Meta -->
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Carson Ng">
+    <meta name="author" content="Carson Ng">
+    <link rel="shortcut icon" href="../favicon2.ico">
+
+    <link href='https://fonts.googleapis.com/css?family=Lato:300,400,300italic,400italic' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
+
+    <!-- FontAwesome JS -->
+    <script defer src="../assets/fontawesome/js/all.js"></script>
+
+    <!-- Global CSS -->
+    <link rel="stylesheet" href="../assets/plugins/bootstrap/css/bootstrap.min.css">
+
+    <!-- github calendar css -->
+    <link rel="stylesheet" href="../assets/plugins/github-calendar/dist/github-calendar-responsive.css">
+    <!-- github activity css -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/octicons/2.0.2/octicons.min.css">
+    <link rel="stylesheet" href="../assets/plugins/github-activity/src/github-activity.css">
+
+    <!-- lightgallery css -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/lightgallery@2.5.0/css/lightgallery-bundle.min.css">
+
+    <!-- Theme CSS -->
+    <link id="theme-style" rel="stylesheet" href="../assets/css/styles.css">
+
+</head>
+
+<body data-theme="dark">
+    <div class="container sections-wrapper py-5">
+        <div class="row">
+            <section class="projects section">
+                <div class="section-inner shadow-sm rounded">
+                    <h2 class="heading">Demo</h2>
+                    <div class="content">
+                        <div class="item">
+                            <h3 class="title"><a href="https://eshop.nurse.org.hk/" target="_blank">Customization for Association of Hong Kong Nursing Staff <i class="fas fa-external-link-alt"></i></a> </h3>
+                            <ul>
+                                <li>Design customization of Magento template</li>
+                                <li>Customize Magento default checkout process to use a 3rd-party payment gateway by implementing specific redirection and callback</li>
+                                <li>Generate specific XML format of confirmed order details and call the client-side API with the XML</li>
+                            </ul>
+                        </div>
+                        <div class="item">
+                            <h3 class="title"><a href="https://sensaihonya.jp" target="_blank">Product Catalog CMS <i class="fas fa-external-link-alt"></i></a></h3>
+                            <p>It is based on Joomla! CMS. The backend panel can be demonstrated via video conference or in person.</p>
+                            <p>Username: demo</p>
+                            <p>Password: Testing!</p>
+                        </div>
+                        <div class="item">
+                            <h3 class="title"><a href="/demo/erp" target="_blank">Customized ERP including HRM and Accounting modules <i class="fas fa-external-link-alt"></i></a> </h3>
+                            <p>Username: admin@company.com</p>
+                            <p>Password: 123456</p>
+                        </div>
+                        <div class="item">
+                            <h3 class="title"><a href="https://www.elc.swd.gov.hk/" target="_blank">Social Welfare Department - e-Learning Centre <i class="fas fa-external-link-alt"></i></a> </h3>
+                            <p>It is based on Moodle. The admin access can be demonstrated via video conference or in person.</p>
+                            <p>Guest login: abc</p>
+                            <p>Password: 7js#^oCUtnirE</p>
+                        </div> <!-- <div class="item"> <h3 class="title"><a href="https://ppfmart.com/" target="_blank">Customization for Association of Hong Kong Nursing Staff <i class="fas fa-external-link-alt"></i></a> </h3> <ul> <li>Responsive Design and logic customization on product variant options of Magento template</li> </ul> </div> -->
+                        <div class="item">
+                            <h3 class="title"><a href="/demo/thermometer_probe.pdf" target="_blank">Mobile layout redesign using Figma <i class="fas fa-external-link-alt"></i></a> </h3>
+                            <p>The link leads to a PDF of redesigned mobile layouts that were created using Figma. I closely collaborated with the designer to ensure that the functional design aligns with the client's requirements. The Figma source can be demonstrated via video conference or in person.</p>
+                        </div>
+                        <div class="item">
+                            <h3 class="title">More work can be demonstrated via video conferences or in person, such as:</h3>
+                            <ul>
+                                <li>UI/UX prototypes</li>
+                                <li>Shopping cart customizations</li>
+                                <li>Accepting credit card payments via the Stripe API</li>
+                            </ul>
+                        </div> <!--//item-->
+
+
+                        <a class="btn btn-cta-secondary float-end" target="_blank" href="https://wa.me/+85259173926?text=Hi%20Carson,%20I%20came%20across%20your%20portfolio%20and%20would%20like%20to%20ask%20for%20a%20demo."><i class="fa-brands fa-whatsapp"></i> Ask for further Demo</a>
+                        <div class="clearfix"></div>
+
+                    </div>
+                    <!--//content-->
+                </div>
+                <!--//section-inner-->
+            </section>
+            <!--//section-->
+        </div>
+    </div>
+
+</body>
+
+</html>
