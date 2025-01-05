03282be7b3f9ec31f13042156bc4a1f5c85b2f11
abc@abc.com <carsonng2000@gmail.com>
2025-01-05 23:22:47 +0800

    updated dynamic current year

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,37 +4,6 @@ localStorage.setItem("darkSwitch", "dark");
 
 /* Vanilla RSS - https://github.com/sdepold/vanilla-rss */
 
-/* const rss = new RSS(
-	document.querySelector("#rss-feeds"),
-	//Change this to your own rss feeds
-	"https://feeds.feedburner.com/TechCrunch/startups",
-	{
-		// how many entries do you want?
-		// default: 4
-		// valid values: any integer
-		limit: 3,
-
-
-		// will request the API via https
-		// default: false
-		// valid values: false, true
-		ssl: true,
-
-		// outer template for the html transformation
-		// default: "<ul>{entries}</ul>"
-		// valid values: any string
-		layoutTemplate: "<div class='items'>{entries}</div>",
-
-		// inner template for each entry
-		// default: '<li><a href="{url}">[{author}@{date}] {title}</a><br/>{shortBodyPlain}</li>'
-		// valid values: any string
-		entryTemplate: '<div class="item"><h3 class="title"><a href="{url}" target="_blank">{title}</a></h3><div><p>{shortBodyPlain}</p><a class="more-link" href="{url}" target="_blank"><i class="fas fa-external-link-alt"></i>Read more</a></div></div>',
-
-	}
-);
-rss.render(); */
-
-
 /* Github Calendar - https://github.com/IonicaBizau/github-calendar */
 //new GitHubCalendar("#github-graph", "IonicaBizau", { responsive: true });
 //new GitHubCalendar("#github-graph", "carsonng", { responsive: true, tooltips: true, cache: 10 });
@@ -518,4 +487,6 @@ $(function () {
 			$('li[data-id="' + $(location).attr('hash').substring(1) + '"] a').trigger('click');
 		}
 	})
+
+	document.getElementById('currentYear').textContent = new Date().getFullYear();
 });
